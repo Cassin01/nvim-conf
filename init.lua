2c3c4db173ba3bcda40e593a65e163440a64d1f9
Cassin01 <koyojb@gmail.com>
2022-03-01 12:42:00 +0900

    fixed

--- init.lua
+++ init.lua
@@ -13,6 +13,16 @@
 --               ╚═╝┴ ┴└─┘└─┘┴┘└┘
 --
 
-vim.cmd("runtime init/plugin/plugin_install.init.vim")
-vim.cmd("runtime init/plugin/plugin_settings.init.vim")
+
+local hotpot_path = vim.fn.stdpath('data')..'/site/pack/packer/start/hotpot.nvim'
+if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
+  print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
+  vim.fn.system({'git', 'clone',
+                 'https://github.com/rktjmp/hotpot.nvim', hotpot_path})
+end
+
+vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
 require('plugins')
+
+require("hotpot")
+require("init")
