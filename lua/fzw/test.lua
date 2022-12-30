5e3a7c1ee53299ba4047dfc3871024772cf2eb09
Cassin01 <koyojb@gmail.com>
2022-12-30 22:02:58 +0900

    fix: fixed a bug caused by typo

---
+++ lua/fzw/test.lua
@@ -0,0 +1,33 @@
+-- local function is_array(t)
+--   local i = 0
+--   for _ in pairs(t) do
+--       i = i + 1
+--       if t[i] == nil then return false end
+--   end
+--   return true
+-- end
+
+-- local d = {}
+-- d["hoge"] = "huga"
+-- d["hog1"] = "huga"
+-- d["hog2"] = "huga"
+
+-- local l = {"a", 2, 3, 4, 5}
+-- local n = {}
+-- for k, v in pairs(d) do
+--   -- table.insert(n, k, v)
+--   n[k] = v
+-- end
+
+-- print(is_array(n))
+-- s = "<hoge"
+-- if string.match(s, "^<Plug>") then
+--   print(true)
+-- end
+-- print(string.match(" <ab>", "^<[%w%-]+>"))
+-- for _ = 1, 1 do
+--   print("a")
+-- end
+
+print(string.sub(" a", 1, 2))
+            -- local sub = string.sub(match.key, 1 + #witch_line, prefix_size + #witch_line)
