295f9175a9e1103eaecb928bb1f8d0d3aedf6c8f
Cassin01 <koyojb@gmail.com>
2023-01-09 20:25:42 +0900

    chore : add user

--- lua/user.lua
+++ lua/user.lua
@@ -1,16 +1,37 @@
 local wf = require("wf")
-wf.setup()
-local which_key = require("wf.builtin.which_key")
+wf.setup({theme="chad"})
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<Space>",
-  "",
-  { callback = which_key("<Space>"), noremap = true, silent = true, desc = "which-key-space", nowait = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "s",
-  "",
-  { callback = which_key("s"), noremap = true, silent = true, desc = "which-key-s", nowait = true }
-)
+local which_key = require("wf.builtin.which_key")
+local register = require("wf.builtin.register")
+local bookmark = require("wf.builtin.bookmark")
+
+vim.api.nvim_set_keymap("n", "<Space>", "", {
+  callback = which_key({ text_insert_in_advance = "<Space>" }),
+  noremap = true,
+  silent = true,
+  desc = "which-key space",
+  nowait = true,
+})
+vim.api.nvim_set_keymap("n", "<Space>wn", "", {
+  callback = which_key(),
+  noremap = true,
+  silent = true,
+  desc = "which-key",
+  nowait = true,
+})
+vim.api.nvim_set_keymap("n", "s", "", {
+  callback = which_key({ text_insert_in_advance = "s" }),
+  noremap = true,
+  silent = true,
+  desc = "which-key s",
+  nowait = true,
+})
+vim.api.nvim_set_keymap("n", "<Leader>", "", {
+  callback = which_key({ text_insert_in_advance = "<Leader>" }),
+  noremap = true,
+  silent = true,
+  desc = "which-key /",
+  nowait = true,
+})
+vim.api.nvim_set_keymap("n", "<Space>wr", "", { callback = register(), noremap = true, silent = true, desc = "wf.nvim register" })
+vim.api.nvim_set_keymap("n", "<Space>wb", "", { callback = bookmark({}), noremap = true, silent = true, desc = "wf.nvim bookmark" })
